c210dfc02b7828acde392a74e636351828a77c68
342939352 <342939352@C-MXL2393F20.tdsb.on.ca>
2024-09-20 11:39:43 -0400

    Adding to ICS3U project with AboutMe class.

---
+++ ICS3U ButerinM/src/unit1/AboutMe.java
@@ -0,0 +1,20 @@
+package unit1;
+/**
+ * Description: This program prints basic information about me.
+ * Date: September 20, 2024
+ * @author Misha Buterin
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// The next six lines have a title and information about me.
+		System.out.println("About Me:");
+		System.out.println("---------");
+		System.out.println("Name - Misha Buterin");
+		System.out.println("Grade - 11");
+		System.out.println("Teacher - Ms. Kemp");
+		System.out.println("School - Lawrence Park Collegiate Institute");
+
+	}
+
+}
