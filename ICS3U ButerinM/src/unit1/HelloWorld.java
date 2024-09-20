a7fb332e2836ac268e197e2954b22b129f1542fe
342939352 <342939352@C-MXL2393F20.tdsb.on.ca>
2024-09-20 11:11:18 -0400

    Adding ICS3U project with HelloWorld class.

---
+++ ICS3U ButerinM/src/unit1/HelloWorld.java
@@ -0,0 +1,19 @@
+package unit1;
+/**
+ * Description: This program prints Hello World, and a second statement. <br>
+ * Date: September 19, 2024
+ * @author Misha Buterin
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// This prints hello to the system console
+		System.out.println("Hello World!!");
+		System.out.println("Hi I'm Misha");
+		/*
+		 * This is a block comment.
+		 * You can add notes on multiple lines.
+		 */
+	}
+
+}
