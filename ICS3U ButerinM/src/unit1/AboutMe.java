b1f2ac9658ee193ddcafe1d24e63563918d88f18
342939352 <342939352@C-MXL2393F20.tdsb.on.ca>
2024-09-23 09:44:41 -0400

    Started creating timetable for About Me class.

--- ICS3U ButerinM/src/unit1/AboutMe.java
+++ ICS3U ButerinM/src/unit1/AboutMe.java
@@ -8,12 +8,14 @@ public class AboutMe {
 
 	public static void main(String[] args) {
 		// The next six lines have a title and information about me.
-		System.out.println("About Me:");
-		System.out.println("---------");
+		System.out.println("About Me: \n---------");
 		System.out.println("Name - Misha Buterin");
 		System.out.println("Grade - 11");
 		System.out.println("Teacher - Ms. Kemp");
 		System.out.println("School - Lawrence Park Collegiate Institute");
+		System.out.println("\n\"Go Panthers!!\"\n");
+		System.out.println("Timetable for the week of Sept. 23 \n----------------------------------");
+		System.out.println("Day");
 
 	}
 
