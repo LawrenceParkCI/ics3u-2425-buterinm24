3825985ff2e88225e9851d0d8c75307ef5155e46
342939352 <342939352@C-MXL2393F20.tdsb.on.ca>
2024-09-26 11:26:52 -0400

    Created and completed ICS3U Signature class, including a picture too.

---
+++ ICS3U ButerinM/src/unit1/Signature.java
@@ -0,0 +1,29 @@
+package unit1;
+/**
+ * Description: This program prints a signature of my name.
+ * Date: September 26, 2024
+ * @author Misha Buterin
+ */
+public class Signature {
+
+	public static void main(String[] args) {
+		// The next 7 lines print a signature of my name.
+		System.out.println("+------------------------------------------------------------------------------------------------+");
+		System.out.println("| ,--.   ,--.,--.       ,--.                 ,-----.            ,--.                ,--.         |");
+		System.out.println("| |   `.'   |`--' ,---. |  ,---.  ,--,--.    |  |) /_ ,--.,--.,-'  '-. ,---. ,--.--.`--',--,--,  |");
+		System.out.println("| |  |'.'|  |,--.(  .-' |  .-.  |' ,-.  |    |  .-.  \\|  ||  |'-.  .-'| .-. :|  .--',--.|      \\ |");
+		System.out.println("| |  |   |  ||  |.-'  `)|  | |  |\\ '-'  |    |  '--' /'  ''  '  |  |  \\   --.|  |   |  ||  ||  | |");
+		System.out.println("| `--'   `--'`--'`----' `--' `--' `--`--'    `------'  `----'   `--'   `----'`--'   `--'`--''--' |");
+		System.out.println("+------------------------------------------------------------------------------------------------+");
+		System.out.println("                    _");
+		System.out.println("                  _(_)_                          wWWWw   _");
+		System.out.println("      @@@@       (_)@(_)   vVVVv     _     @@@@  (___) _(_)_");
+		System.out.println("     @@()@@ wWWWw  (_)\\    (___)   _(_)_  @@()@@   Y  (_)@(_)");
+		System.out.println("      @@@@  (___)     `|/    Y    (_)@(_)  @@@@   \\|/   (_)\\");
+		System.out.println("       /      Y       \\|    \\|/    /(_)    \\|      |/      |");
+		System.out.println("    \\ |     \\ |/       | / \\ | /  \\|/       |/    \\|      \\|/");
+		System.out.println("    \\\\|//   \\\\|///  \\\\\\|//\\\\\\|/// \\|///  \\\\\\|//  \\\\|//  \\\\\\|// ");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+	}
+
+}
