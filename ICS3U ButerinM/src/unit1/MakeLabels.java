e18cb233384c9c2e3b3b18a71c003fea51c13f9b
342939352 <342939352@C-MXL2393F20.tdsb.on.ca>
2024-10-02 11:20:48 -0400

    Added comments to ICS3U MakingLabels class.

--- ICS3U ButerinM/src/unit1/MakeLabels.java
+++ ICS3U ButerinM/src/unit1/MakeLabels.java
@@ -15,16 +15,16 @@ public class MakeLabels {
  */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		// Declaring variables and getting user input.
 		String name;
 		System.out.println("Type in your name and press <Enter>");
 		name = sc.nextLine();
 		
 		String subject;
-		
 		System.out.println("Type in the subject and press <Enter>");
 		subject = sc.nextLine();
 		
+		// Printing label.
 		System.out.println();
 		System.out.println("******************************");
 		System.out.println(name);
