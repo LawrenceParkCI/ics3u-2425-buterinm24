9abe7981719780e591c690c93c31b734f6bc9872
342939352 <342939352@C-MXL2393F20.tdsb.on.ca>
2024-10-03 10:12:56 -0400

    Starting ICS3U PlayingWithStrings class.

---
+++ ICS3U ButerinM/src/unit1/PlayingWithStrings.java
@@ -0,0 +1,19 @@
+package unit1;
+/**
+ * Description:
+ * Date: October 3, 2024
+ * @author Misha Buterin
+ */
+public class PlayingWithStrings {
+/**
+ * Entry point to the program.
+ * @param args
+ */
+	public static void main(String[] args) {
+		String fruit = "apple";
+		fruit = "banana";
+		System.out.println(fruit + "split");
+
+	}
+
+}
