8d514d528968de48ef27e96cf786529c3f3198a1
m <m@LAPTOP-EGUPC11H.lan>
2024-11-01 17:24:19 -0400

    Completed OddEven and PositiveNegative classes.

--- ICS3U ButerinM/src/unit2/OddEven.java
+++ ICS3U ButerinM/src/unit2/OddEven.java
@@ -14,22 +14,22 @@ public class OddEven {
 	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
-		
+
 		//Title
 		System.out.println("ODD OR EVEN?\n------------");
-		
+
 		//Getting user input
-		System.out.print("Type in any integer and press <Enter>: ");
+		System.out.print("Type in an integer of your choice and press <Enter>: ");
 		int num = sc.nextInt();
-		
-		//Testing if it's odd or even and printing output
+
+		//Testing if integer is odd or even and printing output
 		if (num%2 == 0) {
 			System.out.println("Your integer is even.");
 		}
 		else {
 			System.out.println("Your integer is odd.");
 		}
-		
+
 		sc.close();
 
 	}
--- ICS3U ButerinM/src/unit2/PositiveNegative.java
+++ ICS3U ButerinM/src/unit2/PositiveNegative.java
@@ -14,18 +14,35 @@ public class PositiveNegative {
 	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
-		System.out.print("Type in any integer and press <Enter>: ");
+
+		//Title
+		System.out.println("POSITIVE OR NEGATIVE\n--------------------");
+		
+		//Getting user input
+		System.out.print("Type in an integer of your choice and press <Enter>: ");
 		int num = sc.nextInt();
+
+		//Testing if the integer is positive or negative and printing results
 		if (num > 0) {
 			System.out.println("Your integer is positive.");
 		}
-		else {
-			System.out.println("Your integer is negative.");
+		if (num < 0) {
+			System.out.println("Your integer is negative");
 		}
 		
-		if (num%7 == 0) {
-			System.out.println("")
+		//Accounting for zero
+		if (num == 0) {
+			System.out.println("Your integer is neutral.");
 		}
+
+		//Testing if the integer is divisible by 7 and printing results
+		if (num%7 == 0) {
+			System.out.println("Your integer is divisible by seven.");
+		}
+		else {
+			System.out.println("Your integer is not divisible by seven.");
+		}
+		
 		sc.close();
 
 	}
