b43bd8fdbc80fafa9c3ac59c6d7b9bedc9e7bb87
m <m@LAPTOP-EGUPC11H.lan>
2025-01-10 20:07:09 -0500

    Worked on final prokect TaskTrack

--- ICS3U ButerinM/src/finalProject/TaskTrack.java
+++ ICS3U ButerinM/src/finalProject/TaskTrack.java
@@ -16,13 +16,17 @@ public class TaskTrack {
 	 * @param args
 	 */
 	
+	//Initializing console so it works in all methods
+	static Console c;
+	
+	//Initializing arrays so they work in all methods
 	static String[] titles = new String[20];
 	static String[] descriptions = new String [20];
 	static boolean[] isComplete = new boolean[20];
 	
 	public static void main(String[] args) {
 		//Creating console
-		Console c = new Console (30, 150, "TaskTrack");
+		c = new Console (30, 150, "TaskTrack");
 		
 		//CREATING MAIN MENU DISPLAY
 
@@ -75,8 +79,17 @@ public class TaskTrack {
 		c.setColor(new Color (198, 235, 183));
 		c.fillRect(0, 0, c.getWidth(), c.getHeight());
 		
+		c.setColor(Color.BLACK);
+		c.fillRect(460, 485, 300, 90);
+		c.setColor(Color.WHITE);
+		c.fillRect(465, 490, 290, 80);
+		
 		//Case if user enters 1
 		if (answer == 1) {
+			//Drawing ADD A TASK heading
+			c.setColor(Color.BLACK);
+			c.drawString("ADD A TASK", 530, 540);
+			
 			c.println("Alright, let's add a new task to the list!");
 			c.println("Please enter the following information about your task:");
 			c.print("\nTitle of the task: ");
@@ -84,8 +97,9 @@ public class TaskTrack {
 			c.print("\nDescription of the task (if none, just enter \"/\"): ");
 			description = c.readLine();
 			
+			//Calling addTask method to add the task
 			taskCount = addTask(title, description, taskCount);
-			c.println("The task has been successfully added!");
+			c.println("\nThe task has been successfully added!");
 			
 		}
 		else if (answer == 2) {
@@ -103,17 +117,21 @@ public class TaskTrack {
 	}
 	
 	/**
-	 * This method
+	 * This method takes a title and description entered by a user,
+	 * creates a task based on that information, and returns
+	 * an updated count of how many tasks there are.
 	 * @param title -> The title of the task the user wants to add
 	 * @param description -> The description of the task the user wants to add
 	 * @param currentTaskCount -> current number of existing tasks
 	 * @return -> updated count of existing tasks
 	 */
 	public static int addTask (String title, String description, int taskCount) {
+		//Adding info about task to respective arrays
 		titles[taskCount] = title;
 		descriptions[taskCount] = description;
 		isComplete[taskCount] = false;
 		
+		//incrementing task count once the task has been added
 		taskCount++;
 		return taskCount;
 	}
