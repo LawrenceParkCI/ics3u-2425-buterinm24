9931095cf52a13f4f8c5b2c8c24bb9c7867b411a
342939352 <342939352@C-MXL2393F20.tdsb.on.ca>
2025-01-13 10:14:46 -0500

    Worked on final project TaskTrack.

--- ICS3U ButerinM/src/finalProject/TaskTrack.java
+++ ICS3U ButerinM/src/finalProject/TaskTrack.java
@@ -6,8 +6,8 @@ import java.awt.Font;
 import hsa_new.Console;
 
 /**
- * Description:
- * Date: 
+ * Description: This program allows a user to create their own to-do list.
+ * Date: January 15, 2024
  * @author Misha Buterin
  */
 public class TaskTrack {
@@ -15,21 +15,31 @@ public class TaskTrack {
 	 * This is the entry point to the program
 	 * @param args
 	 */
-	
+
 	//Initializing console so it works in all methods
 	static Console c;
-	
+
 	//Initializing arrays so they work in all methods
 	static String[] titles = new String[20];
 	static String[] descriptions = new String [20];
 	static boolean[] isComplete = new boolean[20];
-	
+
 	public static void main(String[] args) {
 		//Creating console
 		c = new Console (30, 150, "TaskTrack");
-		
-		//CREATING MAIN MENU DISPLAY
 
+		//Declaring variables
+		String title = "";
+		String description = "";
+		String ans2;
+		int ans1;
+		int completedTaskNumber; 
+		int taskCount = 0;
+		
+		do {
+		//CREATING MAIN MENU DISPLAY
+		c.clear();
+		
 		//Setting background colour using fillRect
 		c.setColor(new Color (198, 235, 183));
 		c.fillRect(0, 0, c.getWidth(), c.getHeight());
@@ -38,83 +48,96 @@ public class TaskTrack {
 		c.setColor(Color.BLACK);
 		c.setFont(new Font("Serif", Font.BOLD, 40));
 		c.drawString("Welcome to TaskTrack!", 400, 90);
-		
+
 		c.setFont(new Font("Serif", Font.PLAIN, 20));
 		c.drawString("Where would you like to go?", 500, 120);
-		
+
 		//Drawing boxes for each option of where to go
 		c.fillRect(460, 140, 300, 90);
 		c.fillRect(460, 255, 300, 90);
 		c.fillRect(460, 370, 300, 90);
 		c.fillRect(460, 485, 300, 90);
-		
+
 		c.setColor(Color.WHITE);
 		c.fillRect(465, 145, 290, 80);
 		c.fillRect(465, 260, 290, 80);
 		c.fillRect(465, 375, 290, 80);
 		c.fillRect(465, 490, 290, 80);
-		
+
 		//Adding text for each option
 		c.setColor(Color.BLACK);
 		c.setFont(new Font("Serif", Font.BOLD, 27));
-		
+
 		c.drawString("1. ADD A TASK", 510, 195);
 		c.drawString("2. DISPLAY TASKS", 490, 310);
 		c.drawString("3. COMPLETE TASK", 480, 425);
 		c.drawString("4. EXIT", 560, 540);
-		
+
 		//MAIN METHOD CODE 
-		
-		//Declaring variables
-		String title = "";
-		String description = "";
-		int taskCount = 0;
-		
+
 		//Getting user input on where they want to go
 		c.print("Please enter the number of where you would like to go here: ");
-		int answer = c.readInt();
-		
+		ans1 = c.readInt();
+
 		//Clearing screen/keeping background colour
 		c.clear();
 		c.setColor(new Color (198, 235, 183));
 		c.fillRect(0, 0, c.getWidth(), c.getHeight());
-		
+
 		c.setColor(Color.BLACK);
 		c.fillRect(460, 485, 300, 90);
 		c.setColor(Color.WHITE);
 		c.fillRect(465, 490, 290, 80);
-		
+
 		//Case if user enters 1
-		if (answer == 1) {
+		if (ans1 == 1) {
 			//Drawing ADD A TASK heading
 			c.setColor(Color.BLACK);
 			c.drawString("ADD A TASK", 530, 540);
-			
+
 			c.println("Alright, let's add a new task to the list!");
 			c.println("Please enter the following information about your task:");
 			c.print("\nTitle of the task: ");
 			title = c.readLine();
 			c.print("\nDescription of the task (if none, just enter \"/\"): ");
 			description = c.readLine();
-			
+
 			//Calling addTask method to add the task
 			taskCount = addTask(title, description, taskCount);
 			c.println("\nThe task has been successfully added!");
-			
+
 		}
-		else if (answer == 2) {
-			
+		//
+		else if (ans1 == 2) {
+			c.setColor(Color.BLACK);
+			c.drawString("TASKS LIST", 530, 540);
+
+			displayTasks(taskCount);
 		}
-		else if (answer == 3) {
+		//
+		else if (ans1 == 3) {
+			c.print("Ok, please enter the number of the task you would like to complete: ");
+			completedTaskNumber = c.readInt();
+			isComplete[taskCount] = completeTask(completedTaskNumber);
 			
+			c.println("\nThe task has been checked off.");
 		}
-		else if (answer == 4) {
-			
+		else if (ans1 == 4) {
+			c.setColor(Color.BLACK);
+			c.drawString("EXIT", 580, 540);
+
+			c.drawString("Thank you for visiting!", 450, 120);
+			//FIX RETURN TO MAIN MENU ON EXIT OPTION
 		}
 		else {
-			
 		}
+		
+		c.println("Would you like to return to the main menu?");
+		ans2 = c.readLine();
+		
+		} while (ans2.equalsIgnoreCase("yes"));
 	}
+
 	
 	/**
 	 * This method takes a title and description entered by a user,
@@ -122,7 +145,7 @@ public class TaskTrack {
 	 * an updated count of how many tasks there are.
 	 * @param title -> The title of the task the user wants to add
 	 * @param description -> The description of the task the user wants to add
-	 * @param currentTaskCount -> current number of existing tasks
+	 * @param taskCount -> current number of existing tasks
 	 * @return -> updated count of existing tasks
 	 */
 	public static int addTask (String title, String description, int taskCount) {
@@ -130,10 +153,56 @@ public class TaskTrack {
 		titles[taskCount] = title;
 		descriptions[taskCount] = description;
 		isComplete[taskCount] = false;
-		
+
 		//incrementing task count once the task has been added
 		taskCount++;
 		return taskCount;
 	}
+
+	/**
+	 * This method takes the current number of tasks a user has,
+	 * and prints them out using a for loop.
+	 * @param taskCount -> current number of existing tasks
+	 */
+	public static void displayTasks (int taskCount) {
+		//case if the user has no tasks
+		if (taskCount == 0) {
+			c.drawString("You have no tasks to display.", 440, 140);
+		}
+
+		//printing tasks using a for loop
+		for (int i = 0; i < taskCount; i++) {
+			String status;
+			String description;
+			
+			//checking completion status
+			if (isComplete[i]) {
+				status = "[✓]";
+			} else {
+				status = "[ ]";
+			}
+			
+			//checking if there is a description
+			if (descriptions[i].equals("/")) {
+				description = "No description provided";
+			} else {
+				description = descriptions[i];
+			}
+			
+			c.println("\n" + (i + 1) + ".  " + status + titles[i]);	
+			c.println("\t" + description);
+		}
+	}
+	
+	/**
+	 * This method
+	 * @param completedTaskNumber -> the number of the task the user wants to check off
+	 * @return
+	 */
+	public static boolean completeTask (int completedTaskNumber) {
+		
+	}
+	
+	
 
 }
